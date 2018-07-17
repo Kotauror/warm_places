ba4c23af12c748b1d2dc276620266b87ccf4e547
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-07-17 15:03:20 +0100

    Fix: delete unnecessary tests and code.

--- spec/cljs/warm_places/core_spec.cljs
+++ spec/cljs/warm_places/core_spec.cljs
@@ -3,16 +3,6 @@
   (:require [speclj.core]
             [warm_places.core :as my-core]))
 
-(describe "A ClojureScript test"
-  (it "One equals one"
-    (should= 1 1)))
-
-(describe "Counts average"
-  (it "returns average"
-    (should=
-      15
-      (my-core/average 20 10))))
-
 (describe "Creating API query"
   (it "builds a query string"
     (should=
--- src/cljs/warm_places/core.cljs
+++ src/cljs/warm_places/core.cljs
@@ -7,9 +7,6 @@
 
 (enable-console-print!)
 
-(defn average [a b]
-(/ (+ a b) 2.0))
-
 (defn print-to-console [latitude longitude radius]
   (js/console.log "Form was submitted")
   (js/console.log latitude)
@@ -26,9 +23,5 @@
           radius (.-value (.getElementById js/document "radius"))]
     (print-to-console latitude longitude radius)
     (events/prevent-default event)))))
-
-; (defn get-cities []
-;   (go (let [response (<! (http/post "http://http://getnearbycities.geobytes.com/GetNearbyCities?radius=100&latitude=50.058144&longitude=19.959547"))]
-;   js/console.log response)))
 
 (set! (.-onload js/window) add-listener)
