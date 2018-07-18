b9f709ca86ad505672734526c10af26bb54e5c96
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-07-18 17:10:29 +0100

    Feature & Test: update latitude state.

--- spec/cljs/warm_places/core_spec.cljs
+++ spec/cljs/warm_places/core_spec.cljs
@@ -1,13 +1,13 @@
 (ns warm_places.core-spec
   (:require-macros [speclj.core :refer [describe it should=]])
   (:require [speclj.core]
-            [warm_places.core :as my-core]))
+            [warm_places.core :refer [api-url get-city-names update-latitude latitude]]))
 
 (describe "Creating API query"
   (it "builds a query string"
     (should=
       "http://api.geonames.org/findNearbyPlaceNameJSON?lat=50.058144&lng=19.959547&cities=cities1000&radius=100&username=kotaur"
-      (my-core/api-url 50.058144 19.959547 100))))
+      (api-url 50.058144 19.959547 100))))
 
 (def response-json
   (.parse js/JSON "{\"geonames\":[{\"toponymName\": \"Krakow\"}, {\"toponymName\": \"Warszawa\"}]}"))
@@ -16,4 +16,11 @@
   (it "returns all the city names"
     (should=
       ["Krakow" "Warszawa"]
-      (my-core/get-city-names response-json))))
+      (get-city-names response-json))))
+
+(describe "Updates latitude" 
+  (it "updates latitude atom" 
+   (update-latitude "10")
+   (should=
+    "10"
+    @latitude)))
--- src/cljs/warm_places/core.cljs
+++ src/cljs/warm_places/core.cljs
@@ -7,6 +7,8 @@
 
 (enable-console-print!)
 
+(def latitude (atom ""))
+
 (defn api-url [latitude longitude radius]
  (str "http://api.geonames.org/findNearbyPlaceNameJSON?lat=" latitude "&lng=" longitude "&cities=cities1000&radius=" radius "&username=kotaur"))
 
@@ -23,9 +25,21 @@
           longitude (.-value (.getElementById js/document "longitude"))
           radius (.-value (.getElementById js/document "radius"))]))))
 
+(defn update-latitude [value]
+ (swap! latitude (fn [] value)))
+
+(defn value-for-event [event] (.-value (.-target event)))
+
+(defn update-latitude-from-event [event] (update-latitude (value-for-event event)))
+
+(defn add-latitude-input-listener []
+ (.addEventListener (.getElementById js/document "latitude") "change" update-latitude-from-event))
+
 (defn log-json [response]
   (js/console.log response)
   (.then (.json response) get-city-names))
 
-(set! (.-onload js/window) add-listener)
+(set! (.-onload js/window) (fn []
+                            (add-listener)
+                            (add-latitude-input-listener)))
 ; (.then (js/fetch "http://api.geonames.org/findNearbyPlaceNameJSON?lat=50.058144&lng=19.959547&cities=cities1000&radius=100&username=kotaur") log-json)
