b510eb7ea4149b2c965dfe0ca95089264b50e13d
Justyna Zygmunt <justynazygmunt@justynas-air.home>
2018-07-22 14:18:51 +0100

    Update: add comments.

--- src/cljs/warm_places/dom_manipulation.cljs
+++ src/cljs/warm_places/dom_manipulation.cljs
@@ -6,7 +6,13 @@
   (let [li-node (.createElement js/document "li")
         text-node (.createTextNode js/document city)]
         (.appendChild li-node text-node)
+        ; (.addEventListener li-node "click") ;;delete from main add to wish)
         (.appendChild (.getElementById js/document "cities") li-node)))
 
 (defn update-dom [list-of-cities]
   (mapv add-city-to-dom-list list-of-cities))
+
+;; later:
+;; pass to add-city-to-dom-list method of line 9 and contener from line 10.
+
+;;rerender :D deletes stuff and rerender from atoms.
