e32cbd413781d576e5dff765901d56808e44dd5e
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-07-30 11:25:18 +0100

    Refactor: reduce code fragmentation in dom_manipulation ns.

--- src/cljs/warm_places/dom_manipulation.cljs
+++ src/cljs/warm_places/dom_manipulation.cljs
@@ -25,23 +25,30 @@
     (.-innerHTML)
     (set! "")))
 
-(defn add-element-to-dom [city container-name function]
+(defn add-city-to-dom-wishlist [city]
   (let [li-node (.createElement js/document "li")
         text-node (.createTextNode js/document(str (:name city) ": " (:temp city)))]
         (.appendChild li-node text-node)
-        (.addEventListener li-node "click" #(function city))
-        (.appendChild (.getElementById js/document container-name) li-node)))
+        (.addEventListener li-node "click" #(click-in-wishlist-wrapper city))
+        (.appendChild (.getElementById js/document wishlist-list) li-node)))
 
-(defn add-city-to-cities-ul [city]
-  (add-element-to-dom city cities-list click-in-cities-wrapper))
+(defn add-city-to-dom-cities [city]
+  (let [li-node (.createElement js/document "li")
+        text-node (.createTextNode js/document(str (:name city) ": " (:temp city)))]
+        (.appendChild li-node text-node)
+        (.addEventListener li-node "click" #(click-in-cities-wrapper city))
+        (.appendChild (.getElementById js/document cities-list) li-node)))
 
-(defn add-city-to-wishlist-ul [city] 
-  (add-element-to-dom city wishlist-list click-in-wishlist-wrapper))
+;(defn add-city-to-cities-ul [city]
+ ; (add-element-to-dom city cities-list click-in-cities-wrapper))
+
+;(defn add-city-to-wishlist-ul [city] 
+ ; (add-element-to-dom city wishlist-list click-in-wishlist-wrapper))
 
 (defn update-cities-in-dom [list-of-cities]
   (clean-list cities-list)
-  (mapv add-city-to-cities-ul list-of-cities))
+  (mapv add-city-to-dom-cities list-of-cities))
 
 (defn update-wishlist-in-dom [list-of-cities-in-wishlist]
   (clean-list wishlist-list)
-  (mapv add-city-to-wishlist-ul list-of-cities-in-wishlist))
+  (mapv add-city-to-dom-wishlist list-of-cities-in-wishlist))
